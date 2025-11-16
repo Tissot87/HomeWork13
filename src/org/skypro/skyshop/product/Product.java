90a62458a83afe2d0f14362a0b83185518806c35
Kirill <kandriyanoff@yandex.ru>
2025-11-16 22:17:45 +0300

    done

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,23 +1,22 @@
 package org.skypro.skyshop.product;
 
-public class Product {
+public abstract class Product {
 
-    private String name;
+    private final String name;
 
-    private int cost;
 
-    public Product(String name, int cost) {
+    public Product(String name) {
         this.name = name;
-        this.cost = cost;
+
     }
 
     public String getName() {
         return name;
     }
 
-    public int getCost() {
-        return cost;
-    }
+    public abstract int getCost();
+
+    public abstract boolean isSpecial();
 
 
 }
