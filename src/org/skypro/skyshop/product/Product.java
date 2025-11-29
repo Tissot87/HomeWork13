443f9760b7ae42571e18a7f7bb4cb41796b24690
Kirill <kandriyanoff@yandex.ru>
2025-11-29 22:21:11 +0300

    done

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,6 +1,8 @@
 package org.skypro.skyshop.product;
 
-public abstract class Product {
+import org.skypro.skyshop.search.Searchable;
+
+public abstract class Product implements Searchable {
 
     private final String name;
 
@@ -18,5 +20,13 @@ public abstract class Product {
 
     public abstract boolean isSpecial();
 
+    @Override
+    public String searchTerm() {
+        return name;
+    }
 
+    @Override
+    public String contentType() {
+        return "PRODUCT";
+    }
 }
